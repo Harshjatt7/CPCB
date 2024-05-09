9e0f8e808c5e98ad326911764cb4b27602c166ea
Kashish Saxena <kashish.saxena@successive.tech>
2024-05-09 14:12:24 +0530

    Dropdown Field
    Created a common dropdown field for the application.

---
+++ lib/views/widgets/app_components/common_dropdown_text_form_field.dart
@@ -0,0 +1,76 @@
+import 'package:cpcb_tyre/constants/image_constants.dart';
+import 'package:cpcb_tyre/theme/app_color.dart';
+import 'package:cpcb_tyre/views/widgets/components/common_image_widget.dart';
+import 'package:cpcb_tyre/views/widgets/components/common_text_widget.dart';
+import 'package:flutter/material.dart';
+
+class CommonDropdownTextFormField extends StatelessWidget {
+  final List dropDownItem;
+  final String labelText;
+  final void Function(Object?)? onChanged;
+  const CommonDropdownTextFormField(
+      {super.key,
+      required this.labelText,
+      required this.dropDownItem,
+      required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          border: Border.all(
+            color: AppColor().greyD3D3D3,
+          )),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16),
+        child: DropdownButtonFormField(
+          decoration: InputDecoration(
+            isDense: true,
+            border: InputBorder.none,
+            label: RichText(
+              text: TextSpan(
+                  text: labelText,
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelSmall
+                      ?.copyWith(color: AppColor().grey919191),
+                  children: [
+                    TextSpan(
+                      text: "*",
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelSmall
+                          ?.copyWith(color: AppColor().redFF3333),
+                    )
+                  ]),
+            ),
+            suffixIcon: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 17),
+              child: CommonImageWidget(
+                imageSource: ImageConstants().dropdownIcon,
+                isNetworkImage: false,
+              ),
+            ),
+          ),
+          icon: const SizedBox.shrink(),
+          items: dropDownItem
+              .map((label) => DropdownMenuItem(
+                    value: label,
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 2,
+                      ),
+                      child: CommonTextWidget(
+                        label.toString(),
+                        style: Theme.of(context).textTheme.labelSmall,
+                      ),
+                    ),
+                  ))
+              .toList(),
+          onChanged: onChanged,
+        ),
+      ),
+    );
+  }
+}
