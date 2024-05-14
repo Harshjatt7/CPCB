7c2fafaa2ed1004acdb369308f766f428f1ddd64
Kashish Saxena <kashish.saxena@successive.tech>
2024-05-14 10:25:14 +0530

    Developed common type badge widget and  modified the producer's sales list by removing the sized box.

--- lib/views/widgets/app_components/common_producer_sales_list.dart
+++ lib/views/widgets/app_components/common_producer_sales_list.dart
@@ -1,5 +1,6 @@
 import 'package:cpcb_tyre/constants/image_constants.dart';
 import 'package:cpcb_tyre/theme/app_color.dart';
+import 'package:cpcb_tyre/views/widgets/app_components/common_type_badge.dart';
 import 'package:cpcb_tyre/views/widgets/components/common_image_widget.dart';
 import 'package:cpcb_tyre/views/widgets/components/common_text_widget.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +20,7 @@ class ProducerSalesList extends StatelessWidget {
     required this.year,
     required this.month,
     required this.count,
+    required this.producerType,
   });
   final String title;
   final String? motorcycle;
@@ -32,6 +34,7 @@ class ProducerSalesList extends StatelessWidget {
   final String year;
   final String month;
   final String count;
+  final String producerType;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -43,14 +46,21 @@ class ProducerSalesList extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CommonTextWidget(
-            title,
-            style: Theme.of(context).textTheme.displayMedium,
+          Row(
+            children: [
+              CommonTextWidget(
+                title,
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+              const SizedBox(width: 10,),
+              CommonTypeBadge(text: producerType),
+            ],
           ),
           const SizedBox(
             height: 16,
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -63,9 +73,7 @@ class ProducerSalesList extends StatelessWidget {
                   saleItemWidget(context, ImageConstants().bus, bus ?? ""),
                 ],
               ),
-              const SizedBox(
-                width: 44,
-              ),
+              
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -77,9 +85,7 @@ class ProducerSalesList extends StatelessWidget {
                   saleItemWidget(context, ImageConstants().tcv, tcv ?? ""),
                 ],
               ),
-              const SizedBox(
-                width: 44,
-              ),
+              
                Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -91,9 +97,7 @@ class ProducerSalesList extends StatelessWidget {
                   saleItemWidget(context, ImageConstants().tRear, tRear ?? "0"),
                 ],
               ),
-              const SizedBox(
-                width: 44,
-              ),
+             
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
---
+++ lib/views/widgets/app_components/common_type_badge.dart
@@ -0,0 +1,29 @@
+import 'package:cpcb_tyre/theme/app_color.dart';
+import 'package:cpcb_tyre/views/widgets/components/common_text_widget.dart';
+import 'package:flutter/material.dart';
+
+class CommonTypeBadge extends StatelessWidget {
+  const CommonTypeBadge(
+      {super.key, required this.text, this.backgroundColor, this.textColor});
+  final String text;
+  final Color? backgroundColor;
+  final Color? textColor;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: backgroundColor ?? AppColor().lightGreen,
+          borderRadius: BorderRadius.circular(4)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+        child: CommonTextWidget(
+          text,
+          style: Theme.of(context)
+              .textTheme
+              .bodySmall
+              ?.copyWith(color: textColor ?? AppColor().green),
+        ),
+      ),
+    );
+  }
+}
