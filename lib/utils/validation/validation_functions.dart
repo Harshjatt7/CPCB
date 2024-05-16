61f4eff224a98926f1223400140553d96311a331
Harsh <harshpreet.singh@successive.tech>
2024-05-16 18:21:04 +0530

    Add Retreaded Data Screen Validations

--- lib/utils/validation/validation_functions.dart
+++ lib/utils/validation/validation_functions.dart
@@ -8,4 +8,38 @@ class Validations {
       RegExp(r'^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[0-9]{1}[A-Z]{1}[0-9A-Z]{1}$');
   final RegExp dateRegex =
       RegExp(r'^(0[1-9]|[12]\d|3[01])(0[1-9]|1[0-2])\d{2}$');
+
+
+  String? validatePhoneWithCountryCode(String value) {
+    RegExp regex = RegExp(r"^(?:[+0]9)?[0-9]{12}$");
+    final containsUpperCase = value.contains(RegExp(r'[1-9]'));
+    if (!regex.hasMatch(value)) {
+      return 'Please enter a valid phone number';
+    } else if (containsUpperCase == false) {
+      return 'Please enter a valid phone number';
+    } else {
+      return null;
+    }
+  }
+  String? gstValidation(String value) {
+    String pattern =
+        r'^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[0-9]{1}[A-Z]{1}[0-9A-Z]{1}$';
+    RegExp regExp = RegExp(pattern);
+    if (value.toString().isEmpty) {
+      return "Please provide a value";
+    } else if (!regExp.hasMatch(value)) {
+      return "Enter valid GST number";
+    }
+    return null;
+  }
+  String? dateValidation(String value) {
+    String pattern = r'^(((0[1-9])|([12][0-9])|(3[01]))-((0[0-9])|(1[012]))-((20[012]\d|19\d\d)|(1\d|2[3])))';
+    RegExp regExp = RegExp(pattern);
+    if (value.toString().isEmpty) {
+      return "Please provide a Date";
+    } else if (!regExp.hasMatch(value)) {
+      return "Date Format DD-MM-YYYY";
+    }
+    return null;
+  }
 }
