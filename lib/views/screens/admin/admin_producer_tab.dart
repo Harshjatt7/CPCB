063918bfef5b055107c472a04e201c714162dced
Jigyasa Chaudhary <jigyasa.chaudhary@successive.tech>
2024-06-04 11:15:45 +0530

    Feature CPCB dashboard producer tab

--- lib/views/screens/admin/admin_producer_tab.dart
+++ lib/views/screens/admin/admin_producer_tab.dart
@@ -3,125 +3,120 @@ import 'package:cpcb_tyre/constants/string_constant.dart';
 import 'package:cpcb_tyre/theme/app_color.dart';
 import 'package:cpcb_tyre/views/widgets/app_components/common_epr_oblication_tile.dart';
 import 'package:cpcb_tyre/views/widgets/components/common_image_widget.dart';
-import 'package:cpcb_tyre/views/widgets/components/common_single_child_scrollview.dart';
 import 'package:cpcb_tyre/views/widgets/components/common_text_widget.dart';
 import 'package:flutter/material.dart';
 
-class AdminProucerTab extends StatelessWidget {
-  const AdminProucerTab({super.key});
+class AdminProducerTab extends StatelessWidget {
+  const AdminProducerTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CommonSingleChildScrollView(
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 16),
-        child: Column(
-          children: [
-            dashBoardHeading(context, StringConstants().eprOblications,
-                StringConstants.producer),
-            CommonEPROblicationsTile(
-                title: StringConstants().newTyreManufacturers, count: '1321'),
-            CommonEPROblicationsTile(
-                title: StringConstants().newTyreProducedDomestically,
-                count: '1321'),
-            CommonEPROblicationsTile(
-                title: StringConstants().newTyreImported, count: '1321'),
-            CommonEPROblicationsTile(
-                title: StringConstants().newTyreImportedAndImportedVehicles,
-                count: '1321'),
-            CommonEPROblicationsTile(
-                title: StringConstants().newTyreImportedExclusively,
-                count: '1321'),
-            CommonEPROblicationsTile(
-                title: StringConstants().wasteTyreImporter, count: '1321'),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: CommonTextWidget(
-                  StringConstants().totalApplicationReceived,
-                  style: Theme.of(context).textTheme.labelMedium,
-                ),
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Column(
+        children: [
+          dashBoardHeading(context, StringConstants().eprOblications,
+              StringConstants.producer),
+          CommonEPROblicationsTile(
+              title: StringConstants().newTyreManufacturers, count: '1321'),
+          CommonEPROblicationsTile(
+              title: StringConstants().newTyreProducedDomestically,
+              count: '1321'),
+          CommonEPROblicationsTile(
+              title: StringConstants().newTyreImported, count: '1321'),
+          CommonEPROblicationsTile(
+              title: StringConstants().newTyreImportedAndImportedVehicles,
+              count: '1321'),
+          CommonEPROblicationsTile(
+              title: StringConstants().newTyreImportedExclusively,
+              count: '1321'),
+          CommonEPROblicationsTile(
+              title: StringConstants().wasteTyreImporter, count: '1321'),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: CommonTextWidget(
+                StringConstants().totalApplicationReceived,
+                style: Theme.of(context).textTheme.labelMedium,
               ),
             ),
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 16),
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage(ImageConstants().dashboardBackGround),
-                      fit: BoxFit.contain)),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Flexible(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 95, vertical: 40),
-                          child: Container(
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border: Border.all(
-                                    color: AppColor().darkGreen, width: 7)),
-                            padding: const EdgeInsets.all(30),
-                            child: Column(
-                              children: [
-                                CommonImageWidget(
-                                    imageSource: ImageConstants().statsIcon,
-                                    isNetworkImage: false),
-                                CommonTextWidget(
-                                  '4,564',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .labelLarge!
-                                      .copyWith(
-                                          fontSize: 32,
-                                          color: AppColor().darkGreen),
-                                ),
-                                CommonTextWidget(
-                                  StringConstants().totalApplication,
-                                  style: Theme.of(context).textTheme.bodySmall,
-                                )
-                              ],
-                            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 16),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(ImageConstants().dashboardBackGround),
+                    fit: BoxFit.contain)),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 95, vertical: 40),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                  color: AppColor().darkGreen, width: 7)),
+                          padding: const EdgeInsets.all(30),
+                          child: Column(
+                            children: [
+                              CommonImageWidget(
+                                  imageSource: ImageConstants().statsIcon,
+                                  isNetworkImage: false),
+                              CommonTextWidget(
+                                '4,564',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelLarge!
+                                    .copyWith(
+                                        fontSize: 32,
+                                        color: AppColor().darkGreen),
+                              ),
+                              CommonTextWidget(
+                                StringConstants().totalApplication,
+                                style: Theme.of(context).textTheme.bodySmall,
+                              )
+                            ],
                           ),
                         ),
-                      )
-                    ],
-                  )
-                ],
-              ),
+                      ),
+                    )
+                  ],
+                )
+              ],
             ),
-            dashBoardHeading(context, StringConstants().eprOblications,
-                StringConstants.producer),
-            CommonEPROblicationsTile(
-                title: StringConstants().applicationsUnderProcessAtCPCB,
-                count: '131'),
-            CommonEPROblicationsTile(
-                title:
-                    StringConstants().applicationsUnderEvaluationWithDivision,
-                count: '125'),
-            CommonEPROblicationsTile(
-                title: StringConstants().applicationsApprovedByAuthority,
-                count: '131'),
-            CommonEPROblicationsTile(
-                title: StringConstants().applicationsRejectedByAuthority,
-                count: '131'),
-            CommonEPROblicationsTile(
-                title:
-                    StringConstants().applicationsAwaitingApprovalAtAuthority,
-                count: '131'),
-            CommonEPROblicationsTile(
-                title: StringConstants().returnForDivision, count: '131'),
-            CommonEPROblicationsTile(
-                title: StringConstants().applicationPendingAtTheApplicants,
-                count: '131'),
-            CommonEPROblicationsTile(
-                title: StringConstants().numberOfRegistrationGrantedToProducers,
-                count: '131'),
-          ],
-        ),
+          ),
+          dashBoardHeading(context, StringConstants().eprOblications,
+              StringConstants.producer),
+          CommonEPROblicationsTile(
+              title: StringConstants().applicationsUnderProcessAtCPCB,
+              count: '131'),
+          CommonEPROblicationsTile(
+              title: StringConstants().applicationsUnderEvaluationWithDivision,
+              count: '125'),
+          CommonEPROblicationsTile(
+              title: StringConstants().applicationsApprovedByAuthority,
+              count: '131'),
+          CommonEPROblicationsTile(
+              title: StringConstants().applicationsRejectedByAuthority,
+              count: '131'),
+          CommonEPROblicationsTile(
+              title: StringConstants().applicationsAwaitingApprovalAtAuthority,
+              count: '131'),
+          CommonEPROblicationsTile(
+              title: StringConstants().returnForDivision, count: '131'),
+          CommonEPROblicationsTile(
+              title: StringConstants().applicationPendingAtTheApplicants,
+              count: '131'),
+          CommonEPROblicationsTile(
+              title: StringConstants().numberOfRegistrationGrantedToProducers,
+              count: '131'),
+        ],
       ),
     );
   }
